dd8a4575723ac4d295da070cadf471a527387b5c
Family <mh5212357@gmail.com>
2025-10-03 16:02:34 -0400

    commit

--- src/main/java/pluralsight/neighborhoodLibrary.java
+++ src/main/java/pluralsight/neighborhoodLibrary.java
@@ -1,4 +1,59 @@
 package pluralsight;
 
+import java.util.Scanner;
+
 public class neighborhoodLibrary {
+
+    // Initialize an array of book objects with a maximum size of 20.
+    private static Book[] Book = new Book[20];
+    // The number of book currently stored in the vehicles array.
+    private static int numOfBook = 20;
+
+    public static void main(String[] args) {
+        // Create and initialize some book objects and store them in the book array.
+        Book[0] = new Book(1, "139-78-0132350884", "A Handbook of Agile Software Craftsmanship", false, ""  );
+        Book[1] = new Book(2, "13-978-0135957059", "The Pragmatic Programmer: Your Journey to Mastery ");
+        Book[2] = new Book(3, "13: 978-0201616224", "The Pragmatic Programmer: ");
+        // Create a Scanner object to read user input.
+        Scanner scanner = new Scanner(System.in);
+
+        boolean isDone = false;
+
+        // Run a loop until the user quits the program.
+        while (!isDone) {
+
+            // Display the options menu to the user.
+            System.out.println("1. Show Available Books ");
+            System.out.println("2. Show Checked Out Books ");
+            System.out.println("3. Exit - closes out of the application ");
+
+            // Read the user's input command.
+            int comment = scanner.nextInt();
+            // Consume the newline character left in the scanner buffer after reading the command.
+            scanner.nextLine();
+
+            switch (comment) {
+                case 1:
+                    listOfBooks();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("exit");
+                    isDone = true;
+                default:
+                    System.out.println("Book not in the list");
+                    break;
+            }
+
+        }
+
+    }
+    public static void listOfBooks() {
+        System.out.println(" We have books in the list");
+        for (int i = 0; i < numOfBook; i++) {
+
+        }
+
+    }
 }
